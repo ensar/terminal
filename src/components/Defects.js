8ceb3142d7f7530f93a5a4d079da862437b00dc4
Ensar Eken <ensareken987@gmail.com>
2023-02-04 22:17:56 +0300

    defectlist diller eklendi

--- src/components/Defects.js
+++ src/components/Defects.js
@@ -3,32 +3,34 @@ import Header from './Table/Header';
 import Body from './Table/Body';
 import Footer from './Table/Footer';
 import { useDefectsContext } from 'contexts/DefectsContext';
+import { useTranslation } from 'react-i18next';
 
 const Defects = () => {
   const { defects, setDefects } = useDefectsContext();
+  const { t } = useTranslation();
 
   const columns = [
-    { id: 'depCode', label: 'Bildiren' },
-    { id: 'bodyNo', label: 'Body' },
-    { id: 'assyNo', label: 'Assy' },
-    { id: 'vinNo', label: 'Vin No' },
-    { id: 'colorExtCode', label: 'Renk' },
-    { id: 'modelCode', label: 'Mdl' },
-    { id: 'localId', label: 'Sicil' },
-    { id: 'partName', label: 'Parça' },
-    { id: 'spotCode', label: 'Spot' },
-    { id: 'arcnutboltCode', label: 'Gun' },
-    { id: 'arcnutboltCode', label: 'Arc' },
-    { id: 'arcnutboltCode', label: 'ArcGun' },
-    { id: 'defectName', label: 'Hata' },
-    { id: 'defRankCode', label: 'Rank' },
-    { id: 'formattedDefectHour', label: 'Saat' },
-    { id: 'defectType', label: 'Hata Türü' },
-    { id: 'defrespName', label: 'Hata Sor' },
-    { id: 'arcnutboltCode', label: 'Alt sorumlu' },
+    { id: 'depCode', label: t('table.depCode') },
+    { id: 'bodyNo', label: t('table.bodyNo') },
+    { id: 'assyNo', label: t('table.assyNo') },
+    { id: 'vinNo', label: t('table.vinNo') },
+    { id: 'colorExtCode', label: t('table.colorExtCode') },
+    { id: 'modelCode', label: t('table.modelCode') },
+    { id: 'localId', label: t('table.localId') },
+    { id: 'partName', label: t('table.partName') },
+    { id: 'spotCode', label: t('table.spotCode') },
+    { id: 'arcnutboltCode', label: t('table.arcnutboltCode1') },
+    { id: 'arcnutboltCode', label: t('table.arcnutboltCode2') },
+    { id: 'arcnutboltCode', label: t('table.arcnutboltCode3') },
+    { id: 'defectName', label: t('table.defectName') },
+    { id: 'defRankCode', label: t('table.defRankCode') },
+    { id: 'formattedDefectHour', label: t('table.formattedDefectHour') },
+    { id: 'defectType', label: t('table.defectType') },
+    { id: 'defrespName', label: t('table.defrespName') },
+    { id: 'arcnutboltCode', label: t('table.arcnutboltCode4') },
   ];
 
-  const extraColumns = ['NR REASON', 'Kaydet', 'İşlem'];
+  const extraColumns = ['NR REASON', t('table.save'), t('table.operation')];
 
   return (
     <TableContainer sx={{ height: '100vh', overflow: 'auto' }}>
